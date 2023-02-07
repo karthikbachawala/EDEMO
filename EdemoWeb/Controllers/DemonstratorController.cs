b8faf4f24c42446bcffb0039a53e0a0a847aaa81
Karthik Bachawala <1karthik.b@gmail.com>
2023-02-07 11:57:34 +0530

    first commit

---
+++ EdemoWeb/Controllers/DemonstratorController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace EdemoWeb.Controllers
+{
+    [Route("[controller]")]
+    public class DemonstratorController : Controller
+    {
+        private readonly ILogger<DemonstratorController> _logger;
+
+        public DemonstratorController(ILogger<DemonstratorController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
